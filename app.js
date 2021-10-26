ea7c0ece0854ecf029da19ace3196dc1b4c42cbc
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-27 02:53:11 +0600

    My git commit

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 'use stric';
-class Parson{
-    constructor(name, id, school, scclass, status){
-        this.name = name;
-        this.id = id;
-        this.school = school;
-        this.scclass = scclass;
-        this.status = status;
-    };
-    car(){
-        console.log("My car name is " + this.name);
-    }
-};
+const json = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
-// const myInfo = new Parson("Rakibul Islam");
+const pro = JSON.parse(json);
 
-// console.log(`My name is ${myInfo.name}`);
-const par = new Parson("Tasla");
-
-par.car()
+console.log(pro.employees[0].firstName);
