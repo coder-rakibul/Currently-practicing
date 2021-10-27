ab2d3a2194b29df9999cf846fe1a132faf1c98d2
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-27 16:19:38 +0600

    My Json data practis

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-// 'use stric';
-// const employ = {
-//     "name": [
-//         {"firstName": "Rakibul", "lastName": "Islam"},
-//         {"firstName": "Shorna", "lastName": "Islam"},
-//         {"firstName": "Samoli", "lastName": "Islam"}
-//     ]
-// };
-// // Loop
-// employ.name.forEach(function(value){
-//     console.log(value);
-// });
-// // W3School Refarance
-// const json = {
-//     "employees":[
-//       {"firstName":"John", "lastName":"Doe"},
-//       {"firstName":"Anna", "lastName":"Smith"},
-//       {"firstName":"Peter", "lastName":"Jones"}
-//     ]
-// }
+'use stric';
+const employ = {
+    "name": [
+        {"firstName": "Rakibul", "lastName": "Islam"},
+        {"firstName": "Shorna", "lastName": "Islam"},
+        {"firstName": "Samoli", "lastName": "Islam"}
+    ]
+};
+// Loop
+employ.name.forEach(function(value){
+    console.log(value);
+});
+// W3School Refarance
+const json = {
+    "employees":[
+      {"firstName":"John", "lastName":"Doe"},
+      {"firstName":"Anna", "lastName":"Smith"},
+      {"firstName":"Peter", "lastName":"Jones"}
+    ]
+}
 
-// // No responsc with loop
+// No responsc with loop
 
-// const myInfo = {
-//     "name": "Rakibul",
-//     "id": 120,
-//     "addras": "Bangladash",
-//     "myInfo": {
-//         "firstName": "Rakibul",
-//         "lastName": "Islam"
-//     },
-//     "herInfo": {
-//         "firstName": "Shorna",
-//         "lastName": "Islam"
-//     }
-// };
-// myInfo.forEach(function(value){
-//     console.log(value);
-// });
+const myInfo = {
+    "name": "Rakibul",
+    "id": 120,
+    "addras": "Bangladash",
+    "myInfo": {
+        "firstName": "Rakibul",
+        "lastName": "Islam"
+    },
+    "herInfo": {
+        "firstName": "Shorna",
+        "lastName": "Islam"
+    }
+};
+myInfo.forEach(function(value){
+    console.log(value);
+});
 
-// // Dont responce with loop
+// Dont responce with loop
 
 let textJson = '{ "employees" : [' +
 '{ "firstName":"John" , "lastName":"Doe" },' +
