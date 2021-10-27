25da08882cdd7bc8e259783e8571edd6351ad774
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-27 16:18:34 +0600

    My Json data practis

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
-'use stric';
-const json = '{ "employees" : [' +
+// 'use stric';
+// const employ = {
+//     "name": [
+//         {"firstName": "Rakibul", "lastName": "Islam"},
+//         {"firstName": "Shorna", "lastName": "Islam"},
+//         {"firstName": "Samoli", "lastName": "Islam"}
+//     ]
+// };
+// // Loop
+// employ.name.forEach(function(value){
+//     console.log(value);
+// });
+// // W3School Refarance
+// const json = {
+//     "employees":[
+//       {"firstName":"John", "lastName":"Doe"},
+//       {"firstName":"Anna", "lastName":"Smith"},
+//       {"firstName":"Peter", "lastName":"Jones"}
+//     ]
+// }
+
+// // No responsc with loop
+
+// const myInfo = {
+//     "name": "Rakibul",
+//     "id": 120,
+//     "addras": "Bangladash",
+//     "myInfo": {
+//         "firstName": "Rakibul",
+//         "lastName": "Islam"
+//     },
+//     "herInfo": {
+//         "firstName": "Shorna",
+//         "lastName": "Islam"
+//     }
+// };
+// myInfo.forEach(function(value){
+//     console.log(value);
+// });
+
+// // Dont responce with loop
+
+let textJson = '{ "employees" : [' +
 '{ "firstName":"John" , "lastName":"Doe" },' +
 '{ "firstName":"Anna" , "lastName":"Smith" },' +
 '{ "firstName":"Peter" , "lastName":"Jones" } ]}';
 
-const pro = JSON.parse(json);
+const textToJavaScript = JSON.parse(textJson);
 
-console.log(pro.employees[0].firstName);
+textToJavaScript.employees.forEach(function(value){
+    console.log(value.firstName + " " + value.lastName);
+})
