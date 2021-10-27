7eb35d79d689590236d2be9ffb6b19c8364f4c03
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-28 01:52:26 +0600

    Provlem Solving Part two

--- app.js
+++ app.js
@@ -37,3 +37,85 @@ function myName(array, value){
 };
 console.log(myName(['a', 'b', 'c', 'd'], 'c'));
 
+
+
+
+function longWold(names){
+    let longestWold = "";
+    for(let name of names){
+        if(name.length > longestWold.length){
+            longestWold = name;
+        }
+    }
+    return [longestWold, names.indexOf(longestWold)];
+}
+
+
+console.log(longWold(["Rakib", "Rakibul Islma", "Mr Rakibul Islma", "Shikder MD Rakibul Islma", "Rakibul Hosen"]));
+
+function fizzBazz(number){
+
+    for(let i = 1; i <= number; i++){
+        if(i % 15 === 0){
+            console.log(`${i} is fizzBazz`);
+        }else if(i % 3 === 0){
+            console.log(`${i} is fizz`);
+        }else if(i % 5 === 0){
+            console.log(`${i} is Bazz`);
+        }else{
+            console.log(i);
+        };
+    };
+
+};
+
+fizzBazz(100)
+
+
+
+const array = [
+    "Rakibul",
+    12,
+    "Bangladash",
+    false,
+    "Softwor Enginiar",
+    true,
+    NaN,
+    "Thanks",
+    undefined
+];
+
+const proses = array.filter(function(element){
+    if(element){
+        return true;
+    }else{
+        return false;
+    }
+});
+console.log(proses);
+
+// Defolt
+
+const proses2 = array.filter(Boolean);
+console.log(proses2);
+
+const object = {
+    name: "Rakibul",
+    id: 1012,
+    pro: false,
+    addras: "Bangladash",
+    profation: "Freelncer",
+    well: true,
+    deffrent: undefined
+};
+
+const trueObject = function (object){
+    for(let i in object){
+        if(!object[i]){
+            delete object[i]
+        }
+    }
+    return object
+};
+
+console.log(trueObject(object));
