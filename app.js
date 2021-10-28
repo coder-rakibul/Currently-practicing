c80ac42ef6d27a8c35081d1b90e5f1017986053b
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-28 22:30:36 +0600

    ok

--- app.js
+++ app.js
@@ -1,121 +1,19 @@
-const myPoro = `Rakibul Software is a software comparny in Bangladash. it's very big largias comprny.
-                Rakibul Software Athor names is Rakibul Islam. He is a very jinias and lajant boy.
-                Her youube channal names is Rakibul Tachnology
-`;
-
-const machChake = myPoro.match(/Rakibul/ig);
-const lanthOfNumber = machChake ? machChake.length : 0;
-console.log(lanthOfNumber);
-// Rakibul world in Index number
-const myProses = myPoro.search(/Rakibul/i);
-const chackIndex = myProses >= 0 ? myProses : "Not found";
-console.log(chackIndex);
-
-
-// Secend Provelm
-
-let array = [
-    "Rakibul",
-    "Tisa",
-    "Nusrat",
-    "Sada",
-    "Fatama"
-];
-
-for(let x = 0; x < array.length; x++){
-    console.log(array[x]);
-}
-
-function myName(array, value){
-    let length = array.length;
-    for(let x = 0; x < length; x++){
-        if(array[x] === value){
-            return x;
-        };
-    };
-    return "Not found!";
-};
-console.log(myName(['a', 'b', 'c', 'd'], 'c'));
-
-
-
-
-function longWold(names){
-    let longestWold = "";
-    for(let name of names){
-        if(name.length > longestWold.length){
-            longestWold = name;
-        }
+"use strict";
+function Parson(fName, lName, age, proFation){
+    this.fistName = fName;
+    this.lastName = lName;
+    this.age = age;
+    this.status = proFation;
+    this.fullName = function(){
+        return this.fistName + " " + this.lastName;
     }
-    return [longestWold, names.indexOf(longestWold)];
-}
-
-
-console.log(longWold(["Rakib", "Rakibul Islma", "Mr Rakibul Islma", "Shikder MD Rakibul Islma", "Rakibul Hosen"]));
-
-function fizzBazz(number){
-
-    for(let i = 1; i <= number; i++){
-        if(i % 15 === 0){
-            console.log(`${i} is fizzBazz`);
-        }else if(i % 3 === 0){
-            console.log(`${i} is fizz`);
-        }else if(i % 5 === 0){
-            console.log(`${i} is Bazz`);
-        }else{
-            console.log(i);
-        };
-    };
-
 };
 
-fizzBazz(100)
+const rakib = new Parson("Rakibul", "Islam", 12, "Softwor Devloper");
+const shorna = new Parson("Shorna", "Islam", 13, "Home work");
+const nusrat = new Parson("Nusrat", "Kanom", 14, "Jarnalist");
 
+console.log(rakib.fullName());
+console.log(shorna.fullName());
+console.log(nusrat.fullName());
 
-
-const array = [
-    "Rakibul",
-    12,
-    "Bangladash",
-    false,
-    "Softwor Enginiar",
-    true,
-    NaN,
-    "Thanks",
-    undefined
-];
-
-const proses = array.filter(function(element){
-    if(element){
-        return true;
-    }else{
-        return false;
-    }
-});
-console.log(proses);
-
-// Defolt
-
-const proses2 = array.filter(Boolean);
-console.log(proses2);
-
-const object = {
-    name: "Rakibul",
-    id: 1012,
-    pro: false,
-    addras: "Bangladash",
-    profation: "Freelncer",
-    well: true,
-    deffrent: undefined
-};
-
-const trueObject = function (object){
-    for(let i in object){
-        if(!object[i]){
-            delete object[i]
-        }
-    }
-    return object
-};
-
-console.log(trueObject(object));
