e10f0d6bb353cc5b4c148b291644ea23bd9bab08
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-29 02:10:53 +0600

    JavaScript Protoype Object

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 "use strict";
-function Parson(fName, lName, age, proFation){
-    this.fistName = fName;
-    this.lastName = lName;
-    this.age = age;
-    this.status = proFation;
-    this.fullName = function(){
-        return this.fistName + " " + this.lastName;
+// Sub: Prototype
+
+function Parson(name, age) {
+    let parsone = Object.create(Parson.prototype);
+    parsone.nmae = name;
+    parsone.age = age;
+    return parsone;
+};
+
+Parson.prototype = {
+    eat() {
+        console.log(`Parson is eating`);
+    },
+    sleep() {
+        console.log(`Parsone is seleping`);
+    },
+    play() {
+        console.log(`Parsone is plaing`);
     }
+}
+
+const sakib = Parson("Sakib", 16);
+sakib.eat()
+
+console.log(sakib);
+
+const caption = {
+    name: "Masafi",
+    age: 34,
+    cuntry: "Bangladash"
 };
 
-const rakib = new Parson("Rakibul", "Islam", 12, "Softwor Devloper");
-const shorna = new Parson("Shorna", "Islam", 13, "Home work");
-const nusrat = new Parson("Nusrat", "Kanom", 14, "Jarnalist");
-
-Parson.prototype.cuntry = "Bangladash";
-console.log(rakib.cuntry);
-console.log(rakib.fullName());
-console.log(shorna.fullName());
-console.log(nusrat.fullName());
-
-String.prototype.doFun = function(){
-    return 'My name is Rakibul Islam';
-};
-
-
-let my = "Rakibul";
-
-console.log(my.doFun());
-
+const playar = Object.create(caption)
+console.log(playar.name + ", " + playar.age);
