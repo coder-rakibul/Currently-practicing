43b02dc6ea3285c5e8bfc59f210e6f6a83150544
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-10-31 15:38:18 +0600

    My date format

--- app.js
+++ app.js
@@ -1,65 +1,21 @@
-// "use strict";
+"use strict";
 
-// class Car{
-//     constructor(brand){
-//         this.carName = brand;
-//     };
-//     myCar(){
-//         return "I have a " + this.carName;
-//     };
-// };
+const date = new Date();
 
-// class Modale extends Car {
-//     constructor (brad, modle){
-//         super(brad)
-//         this.modleName = modle;
-//     };
-//     static carModle(){
-//         return this.myCar() + "." + " this modle name is " + this.modleName;
-//     };
-// };
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
 
-
-// const findCarName = new Modale("Rolax", 1234);
-
-// console.log(Modale.carModle());
-
-// // class Car {
-// //     constructor(brand) {
-// //       this.carname = brand;
-// //     }
-// //     present() {
-// //       return 'I have a ' + this.carname;
-// //     }
-// //   }
-  
-// //   class Model extends Car {
-// //     constructor(brand, mod) {
-// //       super(brand);
-// //       this.model = mod;
-// //     }
-// //     show() {
-// //       return this.present() + ', it is a ' + this.model;
-// //     }
-// //   }
-  
-// //   let myCar = new Model("Ford", "Mustang");
-  
-// //   console.log(myCar.show());
-
-class Car {
-    constructor(name) {
-      this.name = name;
-    }
-    static hello(name) {
-      return "My car name is " + name + this.name;
-    }
-  }
-  
-  let myCar = new Car("Ford");
-  
-  // You can calll 'hello()' on the Car Class:
-  console.log(Car.hello("Rolax "))
-
-
-  
+const mathFind = months[date.getMonth()];
+console.log(mathFind);
