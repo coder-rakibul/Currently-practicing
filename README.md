2488672ff833938954602b5605d4fb419ff29a84
Rakibul Islam <79575005+shikder-md-rakibul-islam@users.noreply.github.com>
2021-11-02 02:30:44 +0600

    Closd This Corce Now

--- README.md
+++ README.md
@@ -5,5 +5,161 @@
 * [Thapa Tacnical](https://www.youtube.com/watch?v=KGkiIBTq0y0)
 * [W3School.com](https://www.w3schools.com/js)
 
-# Subject: Javascript DOM
+# Subject: All most complet video numbers 81.
+# Next: Document Object Modle [DOM]
+## GitHub Repojatory link: [DOM](https://github.com/coder-rakibul/Lare-with-sumit-JavaScript-DOM)
 ## Document : [Google Docs Link](https://docs.google.com/document/d/1qru2M9_EhBBp4Q98BwjElMcCB12GYWTV96lAOo8gMT0/edit?usp=sharing)
+
+# The nuances of JavaScript
+[Follow w3school](https://w3school.com/js)
+Watch tutorials on each topic (YouTube)
+Participating in the Learn With Sumit quiz.
+
+
+# Basic Section In JavaScript: 
+
+* Basic Concept
+Variable			
+Var let and const
+Operators
+Arithmetic operators
+Assignment operators
+Data type
+Function
+Objects
+Events
+String
+String method
+String search
+Number
+Number method
+Array
+Array method
+Array sort
+Array iteration
+Array const
+Dates format
+Math
+Random
+Boolean
+Comparison Operator
+If else, else, else if
+Switch
+JavaScript Loop [While, for, for in, for of, forElse]
+Brack
+JavaScript Iterables
+Set
+Map
+TypeOf
+Type conversions
+Bitwise Operators
+Regular Expiration
+Error Handling
+Scope
+Hosting
+This Keyword
+Array function
+Classes
+JSON
+Debugging
+JavaScript Version And History
+* JS Version
+JS 2009 (ES5)
+JS 2015 (ES6)
+JS 2016
+JS 2017
+JS 2018
+JS History
+* JavaScript Objects
+Objects Definitions
+Object Properties
+Object Methods
+Object Display
+Object Accessors
+Object Constructors
+Object Prototype
+Object literables
+Object Set
+Object Map
+Object Reference
+* JavaScript Function
+Function Definitions
+Function Parameters
+Function Invocation
+Function Call
+Function Apply
+JavaScript Function Closures
+* JavaScript Classes
+Class Intro
+Class Inheritance
+Static Methods
+* JS Async
+JS Callbacks
+JS Asynchronous
+JS Promises
+JS Async/Await
+* JS HTML DOM
+JS Dom Intro
+
+DOM Methods
+DOM Document
+DOM Element
+DOM HTML
+DOM Form
+DOM CSS
+DOM Animation
+DOM Events
+DOM Event Listener
+DOM Navigation
+DOM Elements (Nodes)
+DOM Collections
+DOM Node Lists
+* JS Browser BOM
+JS Window
+JS Screen
+JS Location
+JS History
+JS Navigato
+JS Popup Alert
+JS Timing
+JS Cookies
+* JS Web APIs
+Web APIs - Introduction
+Web forms API
+Web History API
+Web Storage API
+Web Workers API
+Web Fetch API
+Web Geolocation API
+* JS AJAX
+AJAX Introduction
+AJAX XMLHttp
+AJAX Request
+AJAX Response
+AJAX XML File
+AJAX PHP
+AJAX Database
+AJAX Applications
+AJAX Examples
+* JS JSON
+JSON - Introduction
+JSON Syntax
+JSON vs XML
+JSON Data Types
+JSON.parse()
+JSON.stringify()
+JSON Object
+JSON Array
+JSON Server
+JSON PHP
+JSON HTML
+JSON JSONP
+* JS vs jQuery
+jQuery DOM Selectors
+jQuery HTML
+jQuery CSS
+jQuery CSS
+
+
+
+
--- app.js
+++ app.js
@@ -1,32 +1,158 @@
 "use strict";
 
-const parson = {
-    fullName: function(cuntry, city){
-        return this.fistName + " " + this.lastName + ' ' + cuntry + ' / ' + city;
-    },
-};
 
-const rakibul = {
-    fistName: "Rakibul",
-    lastName: "Islam"
-};
+// All Most Done 81 vedios complit
+// Next: Document Object Modle [DOM]
 
-const shorna = {
-    fistName: "Shorna",
-    lastName: "Islam"
-};
+// The nuances of JavaScript
+// Follow w3school
+// Watch tutorials on each topic (YouTube)
+// Participating in the Learn With Sumit quiz.
 
-const coiti = {
-    fistName: "Coity",
-    lastName: "Hosen"
-};
 
-const scornaN = parson.fullName.apply(shorna, ['Bangladah', 'Dhaka']);
+// Basic Section In JavaScript: 
 
-console.log(scornaN);
+// Basic Concept
+// Variable			
+// Var let and const
+// Operators
+// Arithmetic operators
+// Assignment operators
+// Data type
+// Function
+// Objects
+// Events
+// String
+// String method
+// String search
+// Number
+// Number method
+// Array
+// Array method
+// Array sort
+// Array iteration
+// Array const
+// Dates format
+// Math
+// Random
+// Boolean
+// Comparison Operator
+// If else, else, else if
+// Switch
+// JavaScript Loop [While, for, for in, for of, forElse]
+// Brack
+// JavaScript Iterables
+// Set
+// Map
+// TypeOf
+// Type conversions
+// Bitwise Operators
+// Regular Expiration
+// Error Handling
+// Scope
+// Hosting
+// This Keyword
+// Array function
+// Classes
+// JSON
+// Debugging
+// JavaScript Version And History
+// JS Version
+// JS 2009 (ES5)
+// JS 2015 (ES6)
+// JS 2016
+// JS 2017
+// JS 2018
+// JS History
+// JavaScript Objects
+// Objects Definitions
+// Object Properties
+// Object Methods
+// Object Display
+// Object Accessors
+// Object Constructors
+// Object Prototype
+// Object literables
+// Object Set
+// Object Map
+// Object Reference
+// JavaScript Function
+// Function Definitions
+// Function Parameters
+// Function Invocation
+// Function Call
+// Function Apply
+// JavaScript Function Closures
+// JavaScript Classes
+// Class Intro
+// Class Inheritance
+// Static Methods
+// JS Async
+// JS Callbacks
+// JS Asynchronous
+// JS Promises
+// JS Async/Await
+// JS HTML DOM
+// JS Dom Intro
+// DOM Methods
+// DOM Document
+// DOM Element
+// DOM HTML
+// DOM Form
+// DOM CSS
+// DOM Animation
+// DOM Events
+// DOM Event Listener
+// DOM Navigation
+// DOM Elements (Nodes)
+// DOM Collections
+// DOM Node Lists
+// JS Browser BOM
+// JS Window
+// JS Screen
+// JS Location
+// JS History
+// JS Navigato
+// JS Popup Alert
+// JS Timing
+// JS Cookies
+// JS Web APIs
+// Web APIs - Introduction
+// Web forms API
+// Web History API
+// Web Storage API
+// Web Workers API
+// Web Fetch API
+// Web Geolocation API
+// JS AJAX
+// AJAX Introduction
+// AJAX XMLHttp
+// AJAX Request
+// AJAX Response
+// AJAX XML File
+// AJAX PHP
+// AJAX Database
+// AJAX Applications
+// AJAX Examples
+// JS JSON
+// JSON - Introduction
+// JSON Syntax
+// JSON vs XML
+// JSON Data Types
+// JSON.parse()
+// JSON.stringify()
+// JSON Object
+// JSON Array
+// JSON Server
+// JSON PHP
+// JSON HTML
+// JSON JSONP
+// JS vs jQuery
+// jQuery DOM Selectors
+//  jQuery HTML
+// jQuery CSS
+// jQuery CSS
 
-const aplyMathod = [10, 20, 30, 40, 50, 60];
 
-const wentProses = Math.max.apply(null, aplyMathod);
 
-console.log(wentProses);
+
